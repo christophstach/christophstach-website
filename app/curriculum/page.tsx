afe1e6ddd85cbd2c07c60fd35e07326badb8f8b4
Christoph Stach <christoph.stach@gmail.com>
2022-11-03 12:07:24 +0100

    Added homepage

--- app/curriculum/page.tsx
+++ app/curriculum/page.tsx
@@ -5,14 +5,14 @@ export default function Curriculum() {
   return (
     <div className="flex flex-col lg:flex-row">
       <div className="flex-1">
-        <div className='xl:prose-xl" prose prose-emerald max-w-none dark:prose-invert sm:prose-sm lg:prose-lg mb-10'>
+        <div className='xl:prose-xl" prose prose-emerald mb-10 max-w-none dark:prose-invert sm:prose-sm lg:prose-lg'>
           <h2>Professional Experience</h2>
         </div>
 
         <ExperienceTimeline />
       </div>
       <div className="flex-1">
-        <div className="prose prose-emerald max-w-none dark:prose-invert sm:prose-sm lg:prose-lg xl:prose-xl mb-10">
+        <div className="prose prose-emerald mb-10 max-w-none dark:prose-invert sm:prose-sm lg:prose-lg xl:prose-xl">
           <h2>Education</h2>
         </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,28 @@
 export default function RootPage() {
   return (
     <div className="prose prose-emerald mx-auto dark:prose-invert sm:prose-sm lg:prose-lg xl:prose-xl">
-      <h1>HTML Ipsum Presents</h1>
-
       <p>
-        <strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas.
-        Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
-        egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et
-        sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet,
-        wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac
-        dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.
+        I started software development at the age of 13. It quickly became a passion for me and I started to code my
+        first small projects and websites. I found most interest in websites. These days they were still developed with
+        HTML frame sets and some very basic <a href="https://www.php.net/">PHP</a> scripts.
       </p>
 
-      <h2>Header Level 2</h2>
-
-      <ol>
-        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-        <li>Aliquam tincidunt mauris eu risus.</li>
-      </ol>
-
-      <blockquote>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut
-          a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est
-          malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.
-        </p>
-      </blockquote>
-
-      <h3>Header Level 3</h3>
-
-      <ul>
-        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-        <li>Aliquam tincidunt mauris eu risus.</li>
-      </ul>
-
-      <pre>
-        <code>
-          #header h1 a {'{'}
-          display: block; width: 300px; height: 80px;
-          {'}'}
-        </code>
-      </pre>
+      <p>
+        I kept doing <a href="https://www.php.net/">PHP</a> and also used it in some internships I did. After finishing
+        an IT specialized business school and doing a German apprenticeship, I started my first job as TYPO3-developer
+        (a popular PHP-based CMS-system). I even published some small extensions on the TYPO3 extension gallery. Later
+        on, I switched job to a more generalized <a href="https://www.php.net/">PHP</a> full-stack engineer. After
+        working multiple years, I decided to quit and go traveling for a while. Then when coming back to Germany, I
+        moved to Berlin where I started to studying at <a href="https://www.htw-berlin.de/">HTW Berlin</a>, a little
+        later I started working as a frontend engineer in part-time. During my studies, I also found interest in
+        developing methods of artificial intelligence with neural networks. Later I focused more on methods of
+        bioinformatics, studying the creation of pipelines for next generation sequencing data. I always kept up to date
+        with developing websites. My main interest is the frontend part. I used many frameworks like{' '}
+        <a href="https://angular.io/">Angular</a>, <a href="https://reactjs.org/">React</a>,
+        <a href="https://vuejs.org/">VueJS</a> and <a href="https://tailwindcss.com/">Tailwind CSS</a>. Recently,{' '}
+        <a href="https://svelte.dev/">Svelte</a> (especially Svelte Kit) caught my attention and I also create this
+        website with it.
+      </p>
     </div>
   )
 }
