326ff078741e483bfd1b673553beff7b716750cd
Christoph Stach <christoph.stach@gmail.com>
2022-11-10 23:28:45 +0100

    Remove vercel og

--- src/routes/api/og/(og).tsx
+++ src/routes/api/og/(og).tsx
@@ -1,9 +1,3 @@
-import { ImageResponse } from '@vercel/og'
-
-export const config = {
-  runtime: 'experimental-edge',
-}
-
 export function GET() {
-  return new ImageResponse(<div>TEEEST</div>, { width: 400, height: 300 })
+  return new Response('OG Image test')
 }
