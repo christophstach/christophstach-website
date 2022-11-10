7ad73b32618bd81f45fdb33a75391d3da146f3e2
Christoph Stach <christoph.stach@gmail.com>
2022-11-10 18:39:26 +0100

    Change colors

--- src/components/timeline/TimelineIcon.tsx
+++ src/components/timeline/TimelineIcon.tsx
@@ -8,8 +8,8 @@ export default function TimelineIcon(props: TimelineIconProps) {
   const { children, current } = props
 
   const colors = current
-    ? 'ring-neutral-100 bg-indigo-200 text-indigo-800 dark:bg-indigo-900 dark:text-indigo-200 dark:ring-neutral-900'
-    : 'ring-neutral-100 bg-gray-300 dark:bg-gray-700 dark:ring-neutral-900 '
+    ? 'ring-white bg-indigo-200 text-indigo-800 dark:bg-indigo-900 dark:text-indigo-200 dark:ring-black'
+    : 'ring-white bg-gray-300 dark:bg-gray-700 dark:ring-black '
 
   return (
     <div class={`absolute -left-5 flex h-10 w-10 items-center justify-center rounded-full p-2 ring-8 ${colors}`}>
