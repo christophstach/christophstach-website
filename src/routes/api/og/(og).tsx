a11c86e15391423398d500f484c2965269aaf7b2
Christoph Stach <christoph.stach@gmail.com>
2022-11-10 23:24:18 +0100

    Formating

--- src/routes/api/og/(og).tsx
+++ src/routes/api/og/(og).tsx
@@ -1,8 +1,8 @@
-import { ImageResponse } from '@vercel/og';
+import { ImageResponse } from '@vercel/og'
 
 export const config = {
   runtime: 'experimental-edge',
-};
+}
 
 export function GET() {
   return new ImageResponse(<div>TEEEST</div>, { width: 400, height: 300 })
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import vercel from 'solid-start-vercel'
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
 
-export const config = {
-  runtime: 'experimental-edge',
-}
-
 export default defineConfig({
   plugins: [
     solid({
