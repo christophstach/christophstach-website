a627da27d5f817edf9dae8f9621ddfbc28ad0fb2
Christoph Stach <christoph.stach@gmail.com>
2022-11-10 18:05:28 +0100

    Change to adaptar vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+// @ts-ignore
+import vercel from 'solid-start-vercel'
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [
+    solid({
+      adapter: vercel(),
+    }),
+  ],
 })
