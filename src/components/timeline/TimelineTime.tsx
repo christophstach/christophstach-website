31610c7e6c77241d1e5c8c3f35888157d2c688f4
Christoph Stach <christoph.stach@gmail.com>
2022-11-10 18:56:58 +0100

    Better colors

--- src/components/timeline/TimelineTime.tsx
+++ src/components/timeline/TimelineTime.tsx
@@ -3,5 +3,5 @@ import { ParentProps } from 'solid-js'
 export default function TimelineTime(props: ParentProps) {
   const { children } = props
 
-  return <time class="mb-2 block text-sm font-normal leading-none text-gray-400 dark:text-gray-500">{children}</time>
+  return <time class="mb-2 block text-sm font-normal leading-none text-gray-600 dark:text-gray-400">{children}</time>
 }
