ed2641efdb5664afe295900d397792b9ce364828
Christoph Stach <christoph.stach@gmail.com>
2022-11-10 18:58:37 +0100

    Better colors

--- src/components/timeline/TimelineTitle.tsx
+++ src/components/timeline/TimelineTitle.tsx
@@ -3,5 +3,5 @@ import { ParentProps } from 'solid-js'
 export default function TimelineTitle(props: ParentProps) {
   const { children } = props
 
-  return <h3 class="mb-1 text-lg font-semibold text-white dark:text-black">{children}</h3>
+  return <h3 class="mb-1 text-lg font-semibold text-black dark:text-white">{children}</h3>
 }
