cc8a73b28eeefbc70c3f9fafb3318d4328702e87
Christoph Stach <christoph.stach@gmail.com>
2022-11-13 11:13:29 +0100

    Remove tailwind font config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,15 +1,6 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.tsx'],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
   darkMode: ['class', '[data-theme="dark"]'],
   plugins: [require('@tailwindcss/typography')],
 }
