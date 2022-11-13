01a1904250c9983858c663da1975dd667fc9e2e7
Christoph Stach <christoph.stach@gmail.com>
2022-11-13 10:18:07 +0100

    Aria label for the Open Menu button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -54,6 +54,7 @@ export default function Navbar(props: NavbarProps) {
                   class="rounded-lg p-2.5 text-sm text-indigo-700 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
                   type="button"
                   onclick={handleToggleOpen}
+                  aria-label="Open the menu"
                 >
                   <Show when={open()} fallback={<HiOutlineMenu class="h-5 w-5" />}>
                     <HiOutlineX class="h-5 w-5" />
--- src/components/ThemeSwitch.tsx
+++ src/components/ThemeSwitch.tsx
@@ -11,7 +11,7 @@ export default function ThemeSwitch() {
       type="button"
       class="rounded-lg p-2.5 text-sm text-indigo-700 hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-300 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
       onClick={handleToggleTheme}
-      aria-label="Theme Switch"
+      aria-label="Switch the color scheme"
     >
       <HiOutlineSun class={`h-5 w-5 ${colorScheme() !== 'dark' ? 'hidden' : ''}`} />
       <HiOutlineMoon class={`h-5 w-5 ${colorScheme() === 'dark' ? 'hidden' : ''}`} />
