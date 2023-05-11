c2a45aecda82f66648514078aa6803296ec7d898
Christoph Stach <christoph.stach@gmail.com>
2023-05-11 12:08:25 +0200

    Checkpoint

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-// @ts-ignore
 import vercel from 'solid-start-vercel'
 import solid from 'solid-start/vite'
 import { defineConfig } from 'vite'
@@ -7,7 +6,7 @@ import eslint from 'vite-plugin-eslint'
 export default defineConfig({
   plugins: [
     solid({
-      adapter: vercel(),
+      adapter: vercel({}),
     }),
     eslint(),
   ],
